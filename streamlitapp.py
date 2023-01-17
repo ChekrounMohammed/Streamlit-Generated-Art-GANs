fde16d7e7bdfe814c1e1449c883d75930e00d806
Mohammedpp <chekrouoss@gmail.com>
2023-01-17 08:57:33 +0000

    streamlit three models

--- streamlitapp.py
+++ streamlitapp.py
@@ -13,8 +13,8 @@ Art Gen generates a fake portait using a GAN which learned from a dataset of mor
 You can also control the output by changing the values the noise vector by the Neural Network
 """
 )
-
-model = keras.models.load_model('generator1000.h5')
+activities = ["ART1",'ART2','ART3']
+choice = st.sidebar.selectbox("select a model",activities)
 def generateFace(model):
     
     fig = plt.figure(figsize=(3, 3))
@@ -28,16 +28,29 @@ def generateFace(model):
         plt.imshow((generated_image[0] * 127.5 + 127.5).numpy().astype('uint8'))
         plt.axis('off')
     return fig
+if choice == 'ART1':
+    if st.sidebar.button("Generate New art"):
+        model = keras.models.load_model('generator1000.h5')
+        st.set_option("deprecation.showPyplotGlobalUse", False)
+        st.pyplot(generateFace(model))
+if choice == 'ART3':
+    if st.sidebar.button("Generate New art"):
+        model = keras.models.load_model('Gen3000ep.h5')
+        st.set_option("deprecation.showPyplotGlobalUse", False)
+        st.pyplot(generateFace(model))
+if choice == 'ART2':
+    if st.sidebar.button("Generate New art"):
+        model = keras.models.load_model('Gen2000ep.h5')
+        st.set_option("deprecation.showPyplotGlobalUse", False)
+        st.pyplot(generateFace(model))
 
-if st.sidebar.button("Generate New art"):
-    st.set_option("deprecation.showPyplotGlobalUse", False)
-    st.pyplot(generateFace(model))
+
     
 
 
 
 st.sidebar.markdown(
-    "Get the Source Code [here](https://github.com/ChekrounMohammed/Streamlit-Generated-Art-GANs)",
+    "Get the Source Code [here](https://github.com/SuyashSonawane?tab=repositories)",
     unsafe_allow_html=True,
 )
 
